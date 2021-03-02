18f8c1ead61a13e7d4574d3c67edf74aebfcb578
Uli Köhler <ukoehler@techoverflow.net>
2021-03-03 00:38:44 +0100

    Initial version

---
+++ MyWin10Version.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Management.Automation;
+using System.Reflection;
+using System.Security.Permissions;
+using System.Security.Principal;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MyWin10Version
+{
+    class MyWin10Version
+    {
+        public static void Main(string[] args)
+        {
+            string win10Version = null;
+            using (PowerShell powerShell = PowerShell.Create())
+            {
+                powerShell.AddScript("(Get-WmiObject Win32_OperatingSystem).Version");
+                Collection<PSObject> PSOutput = powerShell.Invoke();
+                StringBuilder stringBuilder = new StringBuilder();
+                foreach (PSObject pSObject in PSOutput)
+                {
+                    stringBuilder.AppendLine(pSObject.ToString());
+                }
+                win10Version = stringBuilder.ToString();
+            }
+            // win10Version == "10.0.19042" ==> extract 19042
+            win10Version = win10Version.Split('.').Last();
+            /**
+             * Show confirmation message
+             */
+            MessageBox.Show("Windows 10 version: " + win10Version, "MyWin10Version", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # MyWin10Version
-One-click .NET program for finding out your current Windows 10 build version.
+Have you ever been looking for a program to just find out which version of Windows 10 you are running? Something without clicking through pages and pages of dialogs?
+
+*MyWin10Version* is for *you* !
+
+## How to run
+
+Download the program from [https://techoverflow.net/downloads/MyWin10Version.exe](https://techoverflow.net/downloads/MyWin10Version.exe) and just run it !
+
+![](https://raw.githubusercontent.com/ulikoehler/MyWin10Version/master/screenshot.png)
+
+## About
+
+This was built by [Uli Köhler](https://techoverflow.net/) who was tired of wasting time telling people how to fix their DNS config.
+Licensed under Apache License v2.0
